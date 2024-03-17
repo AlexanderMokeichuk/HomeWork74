a2938a0b8d0dad9e41c2c5234b75caaba02ac5d0
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-03-17 20:56:46 +0600

    Added check for GET request

--- src/routers/messages.ts
+++ src/routers/messages.ts
@@ -1,12 +1,21 @@
 import express from "express";
+import writeFile from "../writeFile";
+import readDir from "../readDir";
+import {Message} from "../type";
 const messagesRouter = express.Router();
 
-messagesRouter.get('/', (reg, res) => {
-  return res.send({messages: "ss"});
+messagesRouter.get('/', async (reg, res) => {
+  const data: Message[] | undefined = await  readDir();
+  if (data) {
+    return res.send(data);
+  } else {
+    return res.send({error: "Nor found"})
+  }
 });
 
-messagesRouter.post('/', (req, res) => {
-  return res.send({...req.body, datetime: "ХУЙ"});
+messagesRouter.post('/', async (req, res) => {
+  const addMessage = await writeFile(req.body);
+  return res.send(addMessage);
 });
 
 export default messagesRouter;
